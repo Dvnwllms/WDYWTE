4900aeadaba43b0df9ebce0e05c3263d85c782e6
Chris <chrisieaks@gmail.com>
2018-02-14 14:27:04 -0500

    javascript fix

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -2,11 +2,17 @@ var iceCream = ["Chiko", "Tiger Fork", "Doi Moi"];
 var genNum;
 var genSelected;
 
-function generate(resArray) {
-    genNum = Math.floor(Math.random() * resArray.length);
-    genSelected = resArray[genNum];
-    $("#printDiv").html("Why not try out" + genSelected);
-}
+// function generate(resArray) {
+//     genNum = Math.floor(Math.random() * resArray.length);
+//     genSelected = resArray[genNum];
+//     $("#printDiv").html("Why not try out" + genSelected);
+// }
 
-$("#dessertBtn").click(generate(iceCream));
+// $("#dessertBtn").click(generate(iceCream));
 
+
+$("#dessertBtn").on("click", function(){
+    genNum = Math.floor(Math.random() * iceCream.length);
+    genSelected = iceCream[genNum];
+    $("#printDiv").html("Why not try out " + genSelected);
+});
